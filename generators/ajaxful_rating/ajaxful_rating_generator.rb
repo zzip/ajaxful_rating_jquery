4f9ec3773763a7105d1bdcb4f0338c0470b783b7
MrBrdo <mrbrdo@zephyrus.mrbrdo.net>
2010-10-15 01:11:15 +0200

    updated generator to rails 3

--- generators/ajaxful_rating/ajaxful_rating_generator.rb
+++ generators/ajaxful_rating/ajaxful_rating_generator.rb
@@ -1,31 +1,26 @@
-class AjaxfulRatingGenerator < Rails::Generator::NamedBase
+class AjaxfulRatingGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)  
+  include Rails::Generators::Migration
+  
   def initialize(runtime_args, runtime_options = {})
     super
       
     # if there's no user model
     model_file = File.join('app/models', "#{file_path}.rb")
-    raise "User model (#{model_file}) must exits." unless File.exists?(model_file)
+    raise "User model (#{model_file}) must exist." unless File.exists?(model_file)
   end
   
-  def manifest
-    record do |m|
-      m.class_collisions 'Rate'
-      m.template 'model.rb', File.join('app/models', 'rate.rb')
-      m.migration_template 'migration.rb', 'db/migrate',
-        :migration_file_name => 'create_rates'
-      
-      # style
-      m.directory 'public/images/ajaxful_rating'
-      m.file 'images/star.png', 'public/images/ajaxful_rating/star.png'
-      m.file 'images/star_medium.png', 'public/images/ajaxful_rating/star_medium.png'
-      m.file 'images/star_small.png', 'public/images/ajaxful_rating/star_small.png'
-      m.file 'style.css', 'public/stylesheets/ajaxful_rating.css'
-    end
-  end
-  
-  protected
-  
-  def banner
-    "Usage: #{$0} ajaxful_rating UserModelName"
+  def files
+    class_collisions 'Rate'
+    template 'model.rb', File.join('app/models', 'rate.rb')
+    migration_template 'migration.rb', 'db/migrate/create_rates.rb'
+    
+    # style
+    empty_directory 'public/images/ajaxful_rating'
+    
+    copy_file 'images/star.png', 'public/images/ajaxful_rating/star.png'
+    copy_file 'images/star_medium.png', 'public/images/ajaxful_rating/star_medium.png'
+    copy_file 'images/star_small.png', 'public/images/ajaxful_rating/star_small.png'
+    copy_file 'style.css', 'public/stylesheets/ajaxful_rating.css'
   end
 end
